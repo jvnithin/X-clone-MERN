90ea7781ecb1fb4982d7b818ace396688eba6e74
Nithin <jvnithin2583@gmail.com>
2025-02-09 09:29:59 +0530

    first commit

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from "express"
+import {getMe, signup,login,logout } from "../controllers/auth.controller.js"
+import {protectRoute} from "../middleware/protectRoute.js"
+const router=express.Router()
+
+router.post("/signup",signup)
+router.post("/login",login)
+router.post("/logout",logout)
+router.get("/me",protectRoute,getMe)
+export default router;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import {Routes,Route, Navigate} from "react-router-dom";
+import HomePage from './pages/home/HomePage';
+import SignUpPage from './pages/auth/signup/signUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import {Toaster} from "react-hot-toast" 
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
+
+function App() { 
+  const {data:authUser,isLoading}=useQuery({
+    queryKey:['authUser'],
+    queryFn:async ()=>{
+      try {
+        const res=await fetch("/api/auth/me")
+        const data=await res.json();
+        if(data.error) return null;
+        if(!res.ok ){
+          throw new Error(data.error || "Something went wrong")
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry:false,
+  });
+
+  if(isLoading){
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg"/>
+
+      </div>
+    )
+  }
+  return (
+      <div className="flex max-w-6xl mx-auto">
+        {authUser&&<Sidebar/>}
+			<Routes>
+				<Route path='/' element={authUser?<HomePage />:<Navigate to="/login"/>} />
+				<Route path='/login' element={!authUser? <LoginPage />: <Navigate to="/"/>} />
+				<Route path='/signup' element={!authUser?<SignUpPage />:<Navigate to="/"/>} />
+        <Route path='/profile/:username' element={authUser?<ProfilePage />:<Navigate to="/login"/>} />
+        <Route path='/notifications' element={authUser?<NotificationPage />:<Navigate to="/login"/>} />
+			</Routes>
+      {authUser && <RightPanel/>}
+      <Toaster />
+		</div>
+  )
+}
+
+export default App;
